19fc589197c7637de07e8980f8e1c56efbd3fcd4
n2omatt <n2omatt@amazingcow.com>
2018-01-22 23:23:56 -0800

    - Add some macros to identify OS, Processor, Compiler, etc...

--- acow/cpp_goodies.h
+++ acow/cpp_goodies.h
@@ -3,5 +3,6 @@
 // Export Headers                                                             //
 //----------------------------------------------------------------------------//
 #include "include/Disallow.h"
+#include "include/OSMacros.h"
 #include "include/Singleton.h"
 #include "include/SmartPointer.h"
