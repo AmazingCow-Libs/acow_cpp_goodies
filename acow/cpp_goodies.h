d2de326187241f68a3b1093d3c883a7a07e3ac36
n2omatt <n2omatt@amazingcow.com>
2018-01-25 16:23:35 -0800

    - Update the headers.

--- acow/cpp_goodies.h
+++ acow/cpp_goodies.h
@@ -1,8 +1,32 @@
+//~---------------------------------------------------------------------------//
+//                     _______  _______  _______  _     _                     //
+//                    |   _   ||       ||       || | _ | |                    //
+//                    |  |_|  ||       ||   _   || || || |                    //
+//                    |       ||       ||  | |  ||       |                    //
+//                    |       ||      _||  |_|  ||       |                    //
+//                    |   _   ||     |_ |       ||   _   |                    //
+//                    |__| |__||_______||_______||__| |__|                    //
+//                             www.amazingcow.com                             //
+//  File      : cpp_goodies.h                                                 //
+//  Project   : acow_cpp_goodies                                              //
+//  Date      : Jan 20, 2018                                                  //
+//  License   : GPLv3                                                         //
+//  Author    : n2omatt <n2omatt@amazingcow.com>                              //
+//  Copyright : AmazingCow - 2018                                             //
+//                                                                            //
+//  Description :                                                             //
+//                                                                            //
+//---------------------------------------------------------------------------~//
 
 //----------------------------------------------------------------------------//
 // Export Headers                                                             //
 //----------------------------------------------------------------------------//
+#include "include/Deprecated.h"
 #include "include/Disallow.h"
-#include "include/OSMacros.h"
 #include "include/Singleton.h"
 #include "include/SmartPointer.h"
+
+// Discovery Macros.
+#include "include/OSMacros.h"
+#include "include/ProcessorMacros.h"
+#include "include/CompilerMacros.h"
