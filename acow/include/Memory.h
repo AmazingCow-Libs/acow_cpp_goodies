513db8d43a5630b035d9044daff83038e31661ad
n2omatt <n2omatt@amazingcow.com>
2018-02-26 06:02:29 -0300

    - Add some macros to delete stuff.

--- acow/include/Memory.h
+++ acow/include/Memory.h
@@ -5,7 +5,6 @@
 
 namespace acow {
 
-
 template<typename T, typename ...Args>
 inline std::unique_ptr<T> make_unique( Args&& ...args )
 {
@@ -13,3 +12,29 @@ inline std::unique_ptr<T> make_unique( Args&& ...args )
 }
 
 } // namespace acow
+
+
+#define ACOW_SAFE_DELETE(_ptr_) \
+    do {                        \
+        if(_ptr_) {             \
+            delete _ptr_;       \
+            _ptr_ = nullptr;    \
+        }                       \
+    } while(0)
+
+#define ACOW_SAFE_DELETE_ARR(_ptr_) \
+    do {                            \
+        if(_ptr_) {                 \
+            delete [] _ptr_;        \
+            _ptr_ = nullptr;        \
+        }                           \
+    } while(0)
+
+#define ACOW_SAFE_FREE          \
+    do {                        \
+        if(_ptr_) {             \
+            free(_ptr_);        \
+            _ptr_ = nullptr;    \
+        }                       \
+    } while(0)
+
