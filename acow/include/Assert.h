9d6b18aa7330b672f0964ec377163a0de7a5af55
n2omatt <n2omatt@amazingcow.com>
2018-04-16 09:47:27 -0700

    - Add some assertions macros.

---
+++ acow/include/Assert.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// AmazingCow Libs
+#include "acow/c_goodies.h"
+
+
+#define ACOW_ASSERT_STDSTR_NOT_EMPTY(_str_) \
+    ACOW_ASSERT_CSTR_NOT_EMPTY(_str_.c_str())
+
+#define ACOW_ASSERT_NOT_STDSTR_EMPTY_OR_WHITESPACE(_str_) \
+    ACOW_ASSERT_NOT_CSTR_EMPTY_OR_WHITESPACE(_str_.c_str())
