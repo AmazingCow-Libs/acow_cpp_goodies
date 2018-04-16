6959161274aeeb03234035dd70d22e6d2250c53e
n2omatt <n2omatt@amazingcow.com>
2018-04-16 09:48:36 -0700

    - Add the version file.

---
+++ acow/include/Version.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//----------------------------------------------------------------------------//
+// Version                                                                    //
+//----------------------------------------------------------------------------//
+#define ACOW_CPP_GOODIES_VERSION_MAJOR    "1"
+#define ACOW_CPP_GOODIES_VERSION_MINOR    "0"
+#define ACOW_CPP_GOODIES_VERSION_REVISION "0"
+
+#define ACOW_CPP_GOODIES_VERSION           \
+        ACOW_CPP_GOODIES_VERSION_MAJOR "." \
+        ACOW_CPP_GOODIES_VERSION_MINOR "." \
+        ACOW_CPP_GOODIES_VERSION_REVISION
